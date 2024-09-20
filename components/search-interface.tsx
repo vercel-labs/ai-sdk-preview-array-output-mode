8c8d0a98c13e45f7257905d21ae0f78a8cf72ade
Nico Albanese <nico.albanese@vercel.com>
2024-09-20 11:05:58 +0100

    updates

--- components/search-interface.tsx
+++ components/search-interface.tsx
@@ -7,6 +7,9 @@ import { vacationsSchema, vacationSchemaObject } from "@/lib/schema";
 import { useState } from "react";
 import { VacationCard } from "./vacation-card";
 import ProjectOverview from "./project-overview";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { GitIcon, VercelIcon } from "./icons";
 
 export function SearchInterface() {
   const [outputMode, setOutputMode] = useState("array");
@@ -31,7 +34,7 @@ export function SearchInterface() {
   const isLoading = isLoadingArray || isLoadingObject;
 
   return (
-    <div className="min-h-screen bg-neutral-900 text-foreground pt-12 max-w-5xl mx-auto">
+    <div className="min-h-screen bg-neutral-900 text-foreground pt-12 max-w-5xl mx-auto flex justify-center">
       <div className="px-8">
         <div className="w-full mx-auto space-y-8">
           <div className="space-y-4">
@@ -75,6 +78,30 @@ export function SearchInterface() {
           </div>
         </div>
       </div>
+      <motion.div
+        className="flex flex-row gap-4 items-center justify-between fixed bottom-6 text-xs"
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 1 }}
+      >
+        <Link
+          target="_blank"
+          href="https://github.com/vercel-labs/ai-sdk-preview-array-output-mode"
+          className="flex flex-row gap-2 bg-black text-white items-center border px-2 py-1.5 rounded-md border-zinc-700 hover:bg-zinc-800"
+        >
+          <GitIcon />
+          View Source Code
+        </Link>
+
+        <Link
+          target="_blank"
+          href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2Fvercel-labs%2Fai-sdk-preview-array-output-mode&env=OPENAI_API_KEY&envDescription=OpenAI%20API%20Key%20Needed&envLink=https%3A%2F%2Fplatform.openai.com"
+          className="flex flex-row gap-2 items-center px-2 py-1.5 rounded-md   bg-zinc-100 text-zinc-900 hover:bg-zinc-50"
+        >
+          <VercelIcon size={14} />
+          Deploy with Vercel
+        </Link>
+      </motion.div>
     </div>
   );
 }
